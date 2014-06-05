aa99982bd0fca183217f19067ddd1991c556939f
= <=>
2014-06-06 04:53:29 +0530

    created

---
+++ Registration/iLandingViewController.h
@@ -0,0 +1,17 @@
+//
+//  iLandingViewController.h
+//  Registration
+//
+//  Created by ramesh on 06/06/14.
+//  Copyright (c) 2014 xenovus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iLandingViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UIView *launchLogo;
+@property (nonatomic, strong) IBOutlet UIView *launchFade;
+-(IBAction)fadeButton;
+
+@end
---
+++ Registration/iViewController.h
@@ -0,0 +1,13 @@
+//
+//  iViewController.h
+//  Registration
+//
+//  Created by ramesh on 04/06/14.
+//  Copyright (c) 2014 xenovus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iViewController : UIViewController
+-(void)load;
+@end
