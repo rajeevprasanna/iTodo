df16e674fb33744bc7f19d17961f3a7d41ebd3e1
rajeevprasanna <rajeevprasanna@gmail.com>
2014-06-26 22:23:06 +0530

    Setting badge icon

--- itodo/ParseCronService.m
+++ itodo/ParseCronService.m
@@ -8,11 +8,13 @@
 
 #import "ParseCronService.h"
 #import "ParseDataService.h"
+#import "AppDelegate.h"
 
 @implementation ParseCronService
 
 static NSString *_currentUserName;
-
+static NSString* dateFormatPattern = @"yyyy-MM-dd HH:mm:ss Z";
+static NSDateFormatter *dateFormatter;
 
 +(void)setUserName:(NSString *)loggedInUserName
 {
@@ -28,17 +30,50 @@ static dispatch_once_t once;
     });
 }
 
++(NSDate *)getCurrentDate
+{
+    NSDate *currentDate  = [NSDate date];
+    return currentDate;
+}
+
 +(void)lookForTimeOutTasks
 {
     NSLog(@"checking tasks for timeout ones");
     
-    NSArray *taskList;
-    
-    if(_currentUserName != nil){
-         taskList = [ParseDataService getAlreadyLoadedTasks:_currentUserName];
+    if(dateFormatter == nil){
+        dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setDateFormat:dateFormatPattern];
     }
     
-    NSLog(@"taskList => %@", taskList);
+    NSArray *taskList;
+    if(_currentUserName != nil){
+        taskList = [ParseDataService getAlreadyLoadedTasks:_currentUserName];
+    }
+    
+    NSInteger notificationCount = 0;
+    if(taskList && taskList.count >0){
+        for(NSInteger i =0 ;i< taskList.count;i++){
+            PFObject *listData = taskList[i];
+            NSString *dateString = listData[@"date"];
+            
+            if(dateString && dateString.length > 0){
+                NSDate *dateObject = [dateFormatter dateFromString:dateString];
+                NSDate *currentDate = [self getCurrentDate];
+                
+                if([dateObject compare:currentDate] ==  NSOrderedAscending){
+                    NSLog(@"this one is expired time => date => %@", dateObject);
+                    notificationCount ++;
+                }
+            }
+            
+        }
+    }
+    [self setNotificationOnAppIcon:notificationCount];
+}
+
++(void)setNotificationOnAppIcon:(NSInteger)notificationCount
+{
+    [UIApplication sharedApplication].applicationIconBadgeNumber = notificationCount;
 }
 
 @end
